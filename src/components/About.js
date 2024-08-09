33767ae59079e4d3beee78bbddfa9bb5a63f7361
alexmaragia <maragialex@gmail.com>
2024-08-09 16:33:16 +0300

    react-hooks-import-export-lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { image } from '../data/user';
 
 function About() {
@@ -6,8 +5,9 @@ function About() {
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={image} alt="Portfolio project" />
     </div>
   );
 }
+
 export default About;
